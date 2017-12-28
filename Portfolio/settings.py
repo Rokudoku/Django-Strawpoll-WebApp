76670d4a398c845886089c81c98fb4e7ee9376b6
Jerome Probst <Rokudoku@users.noreply.github.com>
2017-12-28 14:19:46 +1100

    Setup connection with mysql database
    First migration worked! Spent the last few days learning mysql, setting it up and connecting it to django.

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -22,7 +22,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY is currently in secrets.py
+
+#
+# -> SECRET_KEY is currently in secrets.py
+#
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -75,13 +78,9 @@ WSGI_APPLICATION = 'Portfolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
+#
+# -> In secrets
+#
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
