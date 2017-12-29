7d8fcd81b2bafa1d95a993712720a6a1617d4638
Jerome Probst <Rokudoku@users.noreply.github.com>
2017-12-29 14:00:55 +1100

    Finished redoing the django tutorial
    Good revision. Everything with the database works fine... had a few issues with permissions but sorted them out.

--- polls/admin.py
+++ polls/admin.py
@@ -1,5 +1,25 @@
 from django.contrib import admin
 
-from .models import Question
+from .models import Choice, Question
 
-admin.site.register(Question)
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    # When viewing list of existing questions
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+    list_per_page = 20
+
+    # When creating/editing questions
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+    inlines = [ChoiceInline]
+
+admin.site.register(Question, QuestionAdmin)
