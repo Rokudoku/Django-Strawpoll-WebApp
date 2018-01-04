214dbe975b1f3059aeebeafa5a485730f65fa5d6
Jerome Probst <Rokudoku@users.noreply.github.com>
2018-01-05 01:06:37 +1100

    Added tests for total_votes.
    If they are summing correctly and if it displays properly on the index page.

--- polls/tests.py
+++ polls/tests.py
@@ -6,9 +6,12 @@ from django.utils import timezone
 
 from .models import Question, Choice
 
+# Every function/method with a comment that starts with a '*' was copied from the official Django tutorial.
+# The others I created by myself.
+
 def create_question(question_text, days):
     """
-    Create a question with the given `question_text` and published the
+    *Create a question with the given `question_text` and published the
     given number of `days` offset to now (negative for questions published
     in the past, positive for questions that have yet to be published).
     """
@@ -20,14 +23,34 @@ def create_choice(question, choice_text, votes):
     Create a choice with the given 'choice_text' and 'votes' that is part of
     the choice_set of the given Question 'question'.
     """
-    question.choice_set.create(choice_text=choice_text, votes=votes)
+    return Choice.objects.create(question=question, choice_text=choice_text, votes=votes)
 
 
 class QuestionModelTests(TestCase):
 
+    def test_total_votes_two_choices(self):
+        """
+        total_votes() returns 12 for a question with choices of 7 and 5 votes.
+        """
+        question = create_question("question", 0)
+        create_choice(question, "choice1", 7)
+        create_choice(question, "choice2", 5)
+        self.assertEqual(question.total_votes(), 12)
+
+    def test_total_votes_four_choices(self):
+        """
+        total_votes() returns 18 for a question with choices of 0, 5, 6, 7 votes.
+        """
+        question = create_question("question", 0)
+        create_choice(question, "choice1", 0)
+        create_choice(question, "choice2", 5)
+        create_choice(question, "choice3", 6)
+        create_choice(question, "choice4", 7)
+        self.assertEqual(question.total_votes(), 18)
+
     def test_was_published_recently_with_future_question(self):
         """
-        was_published_recently() returns False for questions whose pub_date
+        *was_published_recently() returns False for questions whose pub_date
         is in the future.
         """
         time = timezone.now() + datetime.timedelta(days=30)
@@ -36,7 +59,7 @@ class QuestionModelTests(TestCase):
 
     def test_was_published_recently_with_old_question(self):
         """
-        was_published_recently() returns False for questions whose pub_date
+        *was_published_recently() returns False for questions whose pub_date
         is older than 1 day.
         """
         time = timezone.now() - datetime.timedelta(days=1, seconds=1)
@@ -45,7 +68,7 @@ class QuestionModelTests(TestCase):
 
     def test_was_published_recently_with_recent_question(self):
         """
-        was_published_recently() returns True for questions whose pub_date
+        *was_published_recently() returns True for questions whose pub_date
         is within the last day.
         """
         time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
@@ -57,7 +80,7 @@ class QuestionIndexViewTests(TestCase):
 
     def test_no_questions(self):
         """
-        If no questions exist, an appropriate message is displayed.
+        *If no questions exist, an appropriate message is displayed.
         """
         response = self.client.get(reverse('polls:index'))
         self.assertEqual(response.status_code, 200)
@@ -66,7 +89,7 @@ class QuestionIndexViewTests(TestCase):
 
     def test_past_question(self):
         """
-        Questions with a pub_date in the past are displayed on the
+        *Questions with a pub_date in the past are displayed on the
         index page.
         """
         create_question(question_text="Past question.", days=-30)
@@ -78,7 +101,7 @@ class QuestionIndexViewTests(TestCase):
 
     def test_future_question(self):
         """
-        Questions with a pub_date in the future aren't displayed on
+        *Questions with a pub_date in the future aren't displayed on
         the index page.
         """
         create_question(question_text="Future question.", days=30)
@@ -88,7 +111,7 @@ class QuestionIndexViewTests(TestCase):
 
     def test_future_question_and_past_question(self):
         """
-        Even if both past and future questions exist, only past questions
+        *Even if both past and future questions exist, only past questions
         are displayed.
         """
         create_question(question_text="Past question.", days=-30)
@@ -101,7 +124,7 @@ class QuestionIndexViewTests(TestCase):
 
     def test_two_past_questions(self):
         """
-        The questions index page may display multiple questions.
+        *The questions index page may display multiple questions.
         """
         create_question(question_text="Past question 1.", days=-30)
         create_question(question_text="Past question 2.", days=-5)
@@ -111,11 +134,21 @@ class QuestionIndexViewTests(TestCase):
             ['<Question: Past question 2.>', '<Question: Past question 1.>']
         )
 
+    def test_total_votes_displayed(self):
+        """
+        The total votes of a question are properly displayed on the index page.
+        """
+        question = create_question("question", 0)
+        create_choice(question, "choice1", 17)
+        create_choice(question, "choice2", 22)
+        response = self.client.get(reverse('polls:index'))
+        self.assertContains(response, '39')
+
 
 class QuestionDetailViewTests(TestCase):
     def test_future_question(self):
         """
-        The detail view of a question with a pub_date in the future
+        *The detail view of a question with a pub_date in the future
         returns a 404 not found.
         """
         future_question = create_question(question_text='Future question.', days=5)
@@ -125,7 +158,7 @@ class QuestionDetailViewTests(TestCase):
 
     def test_past_question(self):
         """
-        The detail view of a question with a pub_date in the past
+        *The detail view of a question with a pub_date in the past
         displays the question's text.
         """
         past_question = create_question(question_text='Past Question.', days=-5)
@@ -137,7 +170,7 @@ class QuestionDetailViewTests(TestCase):
 class QuestionResultsViewTests(TestCase):
     def test_future_question(self):
         """
-        The results view of a question with a pub_date in the future
+        *The results view of a question with a pub_date in the future
         returns a 404 not found.
         """
         future_question = create_question(question_text='Future question.', days=5)
@@ -147,7 +180,7 @@ class QuestionResultsViewTests(TestCase):
 
     def test_past_question(self):
         """
-        The results view of a question with a pub_date in the past
+        *The results view of a question with a pub_date in the past
         displays the question's text.
         """
         past_question = create_question(question_text='Past Question.', days=-5)
