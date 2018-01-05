7766a0aacdcd2ac3583bcba8298eb832cb5977ee
Jerome Probst <Rokudoku@users.noreply.github.com>
2018-01-05 16:43:39 +1100

    Have basic pagination.
    Lining up the prev/next/current is the challenge at the moment. Thinking of using '<' '>' buttons.

--- polls/views.py
+++ polls/views.py
@@ -9,15 +9,14 @@ from .models import Choice, Question
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
     context_object_name = 'latest_question_list'
+    paginate_by = 5
 
     def get_queryset(self):
         """
         Return the last five published questions (not including those set to be
         published in the future).
         """
-        return Question.objects.filter(
-            pub_date__lte=timezone.now()
-        ).order_by('-pub_date')[:5]
+        return Question.objects.all().order_by('-pub_date')
 
 
 class DetailView(generic.DetailView):
