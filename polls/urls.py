b906cb73c7b60e034dd80ea60d36633fe91f75d5
Jerome Probst <Rokudoku@users.noreply.github.com>
2018-02-12 13:30:24 +1100

    Setup some basic authentication.
    Login/logout works. Need to figure out how to change the redirect after login. Still uses django admin logout- need to change that.

--- polls/urls.py
+++ polls/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 
 from . import views
 
@@ -11,4 +12,6 @@ urlpatterns = [
     path('create/', views.create_question, name='create'),
     path('<int:pk>/delete/', views.QuestionDelete.as_view(), name='delete'),
     path('about/', views.AboutView.as_view(), name='about'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
